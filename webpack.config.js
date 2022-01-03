b46a026f5bff789cb2653e82f086b6b44917c55a
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2022-01-03 14:09:16 -0500

    clean and bundle files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './assets/js/script.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  devtool: false,
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
+  mode: 'development'
+};
